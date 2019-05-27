f97e0b2ddf841d1fe11929862b1bef702e8737bc
Tony Brown <tony@tonybrown.me>
2019-05-27 05:21:10 -0400

    use getRef and vanilla-tilt

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-/* Using useEffect to save state with localStorage */
-import React, {useState, useEffect} from 'react';
+import React, {useRef, useEffect} from 'react'
+import VanillaTilt from 'vanilla-tilt'
+import './App.css'
 
-import './App.css';
-
-function App() {
-  const initialCount = () => (
-    Number(window.localStorage.getItem('count') || 0)
-  )
-  const [count, setCount] = useState(initialCount)
-  const increment = () => setCount(count + 1)
+function Tilt(props) {
+  const tiltRef = useRef()
   useEffect(() => {
-    window.localStorage.setItem('count', count)
-  }, [count])
-  
+    VanillaTilt.init(tiltRef.current, {
+      max: 25,
+      speed: 400,
+      glare: true,
+      'max-glare': 0.5,
+    })
+    return () => tiltRef.current.vanillaTilt.destroy()
+  }, [])
   return (
-    <div className='App'>
-      <button className='demo' onClick={increment}>
-        {count}
-      </button>
+    <div ref={tiltRef} className='tilt-root'>
+      <div className='tilt-child'>{props.children}</div>
     </div>
   )
 }
+
+function App() {
+  return (
+    <div className='totally-centered'>
+      <Tilt>
+        <div className='totally-centered'>vanilla-tilt.js</div>
+      </Tilt>
+    </div>
+  )
+}
+
+
+
+/* Using useEffect to save state with localStorage */
+// import React, {useState, useEffect} from 'react';
+
+// import './App.css';
+
+// function App() {
+//   const initialCount = () => (
+//     Number(window.localStorage.getItem('count') || 0)
+//   )
+//   const [count, setCount] = useState(initialCount)
+//   const increment = () => setCount(count + 1)
+//   useEffect(() => {
+//     window.localStorage.setItem('count', count)
+//   }, [count])
+  
+//   return (
+//     <div className='App'>
+//       <button className='demo' onClick={increment}>
+//         {count}
+//       </button>
+//     </div>
+//   )
+// }
 
 
 /* Using hooks instead of a class based component */
